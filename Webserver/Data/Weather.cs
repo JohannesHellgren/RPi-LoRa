b3afdb1d2a96b22681ab25e02408dd3f98975a2d
Johannes Hellgren <hellgren.johannes@gmail.com>
2022-10-05 16:51:03 +0200

    Added waether-datamodel and services to the garage database

---
+++ Webserver/Data/Weather.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Webserver.Data 
+{
+    public class Weather 
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        public DateTime Timestamp { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+    }
+}
